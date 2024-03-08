bb711bc87e04b550111a5b707bd289bbee631941
Rushabh <rushabh.agarwal9@gmail.com>
2024-03-08 21:58:56 +0530

    try

--- pysaas/pages/app_add.py
+++ pysaas/pages/app_add.py
@@ -3,10 +3,12 @@ import reflex as rx
 
 def app_add() -> rx.Component:
     return rx.text(
-        """greenadexchange.com, 12345, DIRECT, d75815a79
-        silverssp.com, 9675, RESELLER, 496211
-        blueadexchange.com, XF436, DIRECT
-        orangeexchange.com, 45678, RESELLER
-        silverssp.com, ABE679, RESELLER
-        google.com, pub-4964849575236653, DIRECT, f08c47fec0942fa0"""
+        """
+greenadexchange.com, 12345, DIRECT, d75815a79
+silverssp.com, 9675, RESELLER, 496211
+blueadexchange.com, XF436, DIRECT
+orangeexchange.com, 45678, RESELLER
+silverssp.com, ABE679, RESELLER
+google.com, pub-4964849575236653, DIRECT, f08c47fec0942fa0
+"""
     )
